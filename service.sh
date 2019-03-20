a5055d2e8e04ccabcac966b92e503ba19328da8a
Владимир Свинин <alelyya@gmail.com>
2019-03-20 14:55:50 +0300

    init: Создание нового swap-файла.

---
+++ service.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function init {
+    # Creating new swap file with the initial size of 20Mb.
+	dd if=/dev/zero of=/swapfile bs=1024 count=20480  # bs - 
+    chmod 600 /swapfile  # Only the root user should be able to write and read the file.
+	mkswap /swapfile  #  Setting up the file as swap area.
+	swapon /swapfile  # Activating the swap file.
+
+	echo '/swapfile swap swap defaults 0 0' >> /etc/fstab  # To activate /swapfile after reboot.
+}
