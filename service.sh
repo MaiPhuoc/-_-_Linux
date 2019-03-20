e1bf79b66c9d695fb773a857714f6d0fa013c8f8
Владимир Свинин <alelyya@gmail.com>
2019-03-20 16:26:47 +0300

    check() - проверка размера swap-области; expand() - создание нового swap-файла (размер файла передаётся через 1 аргумент).

--- service.sh
+++ service.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-function init {
-    # Creating new swap file with the initial size of 20Mb.
-    dd if=/dev/zero of=/swapfile bs=1024 count=20480  # bs - 
-    chmod 600 /swapfile  # Only the root user should be able to write and read the file.
-    mkswap /swapfile  #  Setting up the file as swap area.
-    swapon /swapfile  # Activating the swap file.
+function expand {
+    echo 'Expanding swap area...'  #  This should be registred by logger.
 
-    echo '/swapfile swap swap defaults 0 0' >> /etc/fstab  # To activate /swapfile after reboot.
+    newfile='/swapfile'$1
+    dd if=/dev/zero of=$newfile bs=1024 count=$1 #  Creating new swap file to double swap area.
+    chmod 600 $newfile  # Only the root user should be able to write and read the file.
+    mkswap $newfile  #  Setting up the file as swap area.
+    swapon $newfile  # Activating the swap file.
+
+    echo $newfile 'swap swap defaults 0 0' >> /etc/fstab  # To activate new swapfile after reboot.
+
 }
+
+function check {
+
+    swap=$(free -b | grep 'Swap' | tr -s ' ')
+    swapsize=$($swap | cut -d' ' -f1)
+    freesize=$($swap | cut -d' ' -f2)
+
+    if (( freesize > (swapsize / 2) ))
+    then
+        expand swapsize
+    fi
+}
+
+while true
+do 
+    check
+    sleep 30 &
+    wait
+done
